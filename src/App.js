bb2f4847a556569a3163848b74c2e151a328bb02
Marcos Ahumada <marcos.ahumada@example.com>
2019-08-21 18:51:50 -0400

    formulario para cambiar noticias

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component , Fragment} from 'react'
 import Header from './components/Header';
 import ListaNoticia from './components/ListaNoticias';
+import Formulario from './components/Formulario';
 
 
 // import PropTypes from 'prop-types'
@@ -14,8 +15,9 @@ export default class App extends Component {
     this.consultarNoticias();
   }
 
-  consultarNoticias = async () => {
-    const url = `https://newsapi.org/v2/top-headlines?country=mx&category=business&apiKey=d1fc0a1f50434d0bbe8951ba28840129`;
+  consultarNoticias = async (categoria = 'general') => {
+    const url = `https://newsapi.org/v2/top-headlines?country=mx&category=${categoria}&apiKey=d1fc0a1f50434d0bbe8951ba28840129`;
+    console.log('url: ', url);
     const respuesta = await fetch(url);
     const noticias = respuesta.json();
     noticias.then( (resp) => {     
@@ -29,6 +31,7 @@ export default class App extends Component {
       <Fragment>
         <Header titulo="Noticias React API"></Header>
         <div className="container white contenedor-noticias">
+          <Formulario consultarNoticias = {this.consultarNoticias}></Formulario>
           <ListaNoticia noticas = {this.state.noticias }></ListaNoticia>
         </div>
       </Fragment>
--- src/components/Noticia.js
+++ src/components/Noticia.js
@@ -4,15 +4,25 @@ const Noticia = ({noticia}) => {
     // extraer datos
     // eslint-disable-next-line no-unused-vars
     const { urlToImage, url, title, description, source } = noticia;
+    // mostrar imagen si no hay
+    const image = (urlToImage) ? 
+        <div className="card-image">
+            <img src = { urlToImage } alt = { title }></img>
+            <span className="card-title">{source.name}</span>
+        </div>
+    : null;
     return (
         <div className="col s12 m6 l4"> 
             <div className="card">
-                <div className="card-image">
-                    <img src = { urlToImage } alt = { title }></img>
-                </div>
+                {image}
                 <div className="card-content">
                     <h3>{title}</h3>
                     <p>{description}</p>
+                </div>
+                <div className="card-action">
+                    <a href = {url} target ="_black" rel="noopener noreferrer" className="waves-effect waves-light btn">
+                        Ver Noticia
+                    </a>
                 </div>
 
             </div>
